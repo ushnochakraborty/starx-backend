14d27a655e1424824f6a8807f33c2bf410c53f54
Pavel Sherstyuk <54330842+sherpavel@users.noreply.github.com>
2022-11-17 14:20:12 -0500

    server v0.1

--- src/database.js
+++ src/database.js
@@ -1,9 +1,137 @@
 /**
- * Wrapper for SQL database for ease of use
+ * Wrapper for MariaDB database for ease of use
  */
- class Database {
-    static connect() {
 
+/**
+ * @typedef {import("./server").SurveyData} SurveyData
+ */
+
+const mariadb = require("mariadb");
+
+/*
+
+-- create
+CREATE TABLE UIDLOOKUP (
+  uid INTEGER PRIMARY KEY AUTO_INCREMENT,
+  pin SMALLINT NOT NULL
+);
+
+CREATE TABLE RESPONSES (
+  recordId INTEGER PRIMARY KEY AUTO_INCREMENT,
+  uid INTEGER,
+  foreign key (uid) references UIDLOOKUP(uid),
+  q1 TINYINT NOT NULL,
+  q2 TINYINT NOT NULL,
+  q3 TINYINT NOT NULL,
+  q4 TINYINT NOT NULL,
+  q5 TINYINT NOT NULL,
+  q6 TINYINT NOT NULL,
+  q7 TINYINT NOT NULL,
+  q8 TINYINT NOT NULL,
+  q9 TINYINT NOT NULL,
+  q10 TINYINT NOT NULL,
+  q11 TINYINT NOT NULL,
+  q12 TINYINT NOT NULL,
+  q13 TINYINT NOT NULL,
+  q14 TINYINT NOT NULL,
+  q15 TINYINT NOT NULL,
+  q16 TINYINT NOT NULL,
+  q17 TINYINT NOT NULL,
+  q18 TINYINT NOT NULL
+);
+
+-- insert
+INSERT INTO UIDLOOKUP VALUES (1, 1234);
+INSERT INTO UIDLOOKUP VALUES (2, 1234);
+INSERT INTO UIDLOOKUP VALUES (3, 4321);
+INSERT INTO UIDLOOKUP VALUES (4, 1111);
+INSERT INTO RESPONSES VALUES (1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1);
+
+-- fetch 
+SELECT * FROM UIDLOOKUP;
+SELECT * FROM RESPONSES;
+
+*/
+
+ class Database {
+    static isConnected = false;
+    /**
+     * @type {mariadb.Pool}
+     */
+    static pool = null;
+    /**
+     * @type {mariadb.PoolConnection}
+     */
+    static connection = null;
+
+    /**
+     * 
+     * @param {String} host 
+     * @param {String} user 
+     * @param {String} password 
+     * @returns {Promise<Boolean>} connected
+     */
+    static async connect(host, user, password) {
+        if (this.isConnected) return;
+        this.pool = mariadb.createPool({
+            host: host,
+            user: user,
+            password: password,
+            connectionLimit: 2
+        });
+        connection = await this.pool.getConnection().catch(e => {
+            console.error("DB connection error: " + e);
+            return false;
+        });
+        this.isConnected = true;
+        return true;
+    }
+
+    /**
+     * 
+     * @param {Number} id 
+     * @returns {Promise<Boolean>} ID exists
+     */
+     static async #idExists(id) {
+        // TODO!!!
+        // await this.connection.query()
+        return false;
+    }
+
+    /**
+     * 
+     * @param {SurveyData} results
+     * @returns {Promise<Number | Boolean>} ID
+     */
+    static async upload(results) {
+        if (!this.isConnected) return null;
+
+        if (results.id) {
+            if (!(await this.#idExists(id))) return false;
+        } else {
+            let id = 0;
+            do {
+                id = Math.floor(Math.random() * 0xffffff);
+            } while (!(await this.#idExists(id)));
+            results.id = id;
+        }
+        
+        // TODO insert data
+
+        return id;
+    }
+
+    /**
+     * 
+     * @param {String} id 
+     * @param {Number} pin
+     * @returns {Promise<SurveyData?>} success
+     */
+    static async get(id, pin) {
+        if (!this.isConnected) return null;
+        if (!(await this.#idExists(id))) return null;
+
+        return null;
     }
 }
 
--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 /*=== Database ===*/
 require("dotenv").config();
 const {DB} = require("./database");
-DB.connect();
+// DB.connect(process.env.DB_HOST, process.env.DB_USER, process.env.DB_PASS);
 
 /*=== Express ===*/
 const express = require("express");
@@ -9,27 +9,74 @@ const path = require("path");
 const app = express();
 // Middleware stuff
 app.use(express.json());
-app.use(express.urlencoded());
+
+const ENDPOINTS = {
+    SURVEY_GET: '/survey',
+    SURVEY_POST: '/survey',
+};
+
+/**
+ * @typedef {{
+ * id: Number
+ * questions: {
+ * q1: Number,
+ * q2: Number,
+ * q3: Number,
+ * q4: Number,
+ * q5: Number,
+ * q6: Number,
+ * q7: Number,
+ * q8: Number,
+ * q9: Number,
+ * q10: Number,
+ * q11: Number,
+ * q12: Number,
+ * q13: Number,
+ * q14: Number,
+ * q15: Number,
+ * q16: Number,
+ * q17: Number,
+ * q18: Number,
+ * }
+ * }} SurveyData
+ */
 
 // For client-side access (HTML, CSS, JS, Images, Public PDFs, Fonts, etc.)
 app.use('/public', express.static('public'));
 
-// Home endpoint
-app.get('/', (req, res) => {
-    // TODO
-});
-
 // Survey post result
-app.post('/survey', (req, res) => {
-    let data = req.body;
-    // TODO
+app.post(`${ENDPOINTS.SURVEY_POST}`, async (req, res) => {
+    if (!DB.isConnected) {
+        res.status(503).send("Database error");
+        return;
+    }
+    let data = req.body.data;
+    let id = await DB.upload(data);
+    if (!id) {
+        res.sendStatus(400);
+        return;
+    }
+    res.status(201).send(id);
 });
 
 // Survey lookup endpoint
-app.get('/survey/:id', (req, res) => {
-    // Unique identifier for result lookup
+app.get(`${ENDPOINTS.SURVEY_GET}/:id`, async (req, res) => {
+    if (!DB.isConnected) {
+        res.status(503).send("Database error");
+        return;
+    }
     let id = req.params.id;
-    // TODO Lookup and send html or pdf
+    let pin = req.body.pin;
+    if (!pin) {
+        res.status(401).send("No pin");
+        return;
+    }
+    let data = await DB.get(id, pin);
+    if (!data) {
+        res.sendStatus(401);
+        return;
+    }
+    res.status(200).json(data);
 });
 
 // Start server
